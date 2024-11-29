762a9cecbd8008f4f2a4a73b24a00acbab5fde08
Aaron Taylor <aarontaylor@users.noreply.github.com>
2024-11-28 20:58:00 -0500

    Updated LeafNode class and unit tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,13 +10,26 @@ class HTMLNode:
         raise NotImplementedError("This method should be implemented by subclasses")
 
     def props_to_html(self):
-        if not self.props:
+        if self.props is None:
             return ""
-        # Generate HTML attributes as a string
-        return " " + " ".join(f'{key}="{value}"' for key, value in self.props.items())
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
     def __repr__(self):
-        return (
-            f"HTMLNode(tag={self.tag!r}, value={self.value!r}, "
-            f"children={self.children!r}, props={self.props!r})"
-        )
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
